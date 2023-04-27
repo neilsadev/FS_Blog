f9af12def0e3c263a1827a225381d126220877e5
neilsadev <neiloyhassan1@gmail.com>
2023-04-27 09:16:41 +0600

    menu and theme implementation

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,11 +16,35 @@ const scrollHeader = () => {
 
 window.addEventListener('scroll', scrollHeader);
 // Open menu & search pop-up
+const menuToggleIcon = selectElement('#menu-toggle-icon');
 
+const toggleMenu = () => {
+    const mobileMenu = selectElement('#menu');
+    mobileMenu.classList.toggle('activated');
+    menuToggleIcon.classList.toggle('activated');
+};
+
+menuToggleIcon.addEventListener('click', toggleMenu);
 // Open/Close search form popup
 
 // -- Close the search form popup on ESC keypress
 
 // Switch theme/add to local storage
+const bodyElement = document.body;
+const themeToggleBtn = selectElement('#theme-toggle-btn');
+const currentTheme = localStorage.getItem('currentTheme')
 
+if (currentTheme) {
+    bodyElement.classList.add('light-theme');
+}
+
+themeToggleBtn.addEventListener('click', () => {
+    bodyElement.classList.toggle('light-theme');
+
+    if(bodyElement.classList.contains('light-theme')){
+        localStorage.setItem('currentTheme', 'themeActive');
+    }else{
+        localStorage.removeItem('currentTheme');
+    }
+});
 // Swiper
